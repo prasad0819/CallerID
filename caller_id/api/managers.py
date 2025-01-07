c19551cf585a4a75fd46db7d3ca59e549eac0d0b
prasad0819 <prasad0819@gmail.com>
2025-01-07 16:45:09 +0530

    Caller ID API

---
+++ caller_id/api/managers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.base_user import BaseUserManager
+from phonenumber_field.phonenumber import PhoneNumber
+
+
+# CustomUserManager to use phone number instead of username for User
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, phone_number, password, **extra_fields):
+        if not phone_number:
+            raise ValueError("The Phone Number must be set")
+        
+        phone_number = PhoneNumber.from_string(phone_number)
+
+        if not phone_number.is_valid():
+            raise ValueError("The Phone Number is not valid")
+        
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(phone_number, password, **extra_fields)
